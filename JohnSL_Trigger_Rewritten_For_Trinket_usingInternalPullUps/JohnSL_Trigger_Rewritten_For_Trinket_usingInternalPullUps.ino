f4e8de084668a4862cbf3d6389f9c7889ec5a06d
R. Ian Lee, Sr <ian@houseoflees.net>
2014-05-25 10:47:39 -0500

    Removed all loops and optimized for use with FSRs in parallel on a single analog pin .

--- JohnSL_Trigger_Rewritten_For_Trinket_usingInternalPullUps/JohnSL_Trigger_Rewritten_For_Trinket_usingInternalPullUps.ino
+++ JohnSL_Trigger_Rewritten_For_Trinket_usingInternalPullUps/JohnSL_Trigger_Rewritten_For_Trinket_usingInternalPullUps.ino
@@ -24,52 +24,40 @@
 // The end stop output
 #define TRIGGER     01
 
-short fsrPins[] = { 2, 4, 3 };     // Pins for each of the FSR analog inputs
-short fsrAnalogNum[] = { 1, 2, 3 };     // Pins for each of the FSR analog inputs
+short fsrPin = 2;               // Pins for each of the FSR analog inputs
+short fsrAnalogNum = 1;         // Analog pin numbers to use with analogRead()
 
 #define SHORT_SIZE 8
 #define LONG_SIZE 16
 #define LONG_INTERVAL (2000 / LONG_SIZE)
 
-unsigned long lastLongTime[3];              // Last time in millis that we captured a long-term sample
-uint16_t longSamples[3][LONG_SIZE];         // Used to keep a long-term average
-uint8_t longIndex[3] = {0, 0, 0};           // Index of the last long-term sample
-uint16_t longAverage[3] = {0, 0, 0};
+unsigned long lastLongTime;              // Last time in millis that we captured a long-term sample
+uint16_t longSamples[LONG_SIZE];         // Used to keep a long-term average
+uint8_t longIndex = 0;                   // Index of the last long-term sample
+uint16_t longAverage = 0;
 
-uint16_t shortSamples[3][SHORT_SIZE];       // Used to create an average of the most recent samples
-uint8_t averageIndex[3] = {0, 0, 0};
+uint16_t shortSamples[SHORT_SIZE];       // Used to create an average of the most recent samples
+uint8_t averageIndex = 0;
 
 uint8_t pin=0;                              // Defining a global pin variable that is used in the code.
 
-void SetOutput(short fsr, bool state)
+void SetOutput(bool state)
 {
-    static bool triggered[3] = {false};
-
-    triggered[fsr] = state;
-    
-    bool any = false;
-    for (uint8_t fsr = 0; fsr < 3; fsr++)
-    {
-        any |= triggered[fsr];
-    }
-
-    digitalWrite(LEDTRIGGER, any ? HIGH : LOW);
-    digitalWrite(ENDSTOP, any ? HIGH : LOW);
+    static bool triggered = false;
+    triggered = state;
+    digitalWrite(LEDTRIGGER, triggered);
+    digitalWrite(ENDSTOP, triggered);
 }
 
 void InitValues()
 {
-    for (uint8_t fsr = 0; fsr < 3; fsr++)
-    {
-        for (uint8_t i = 0; i < SHORT_SIZE; i++)
-            shortSamples[fsr][i] = 0;
+  for (uint8_t i = 0; i < SHORT_SIZE; i++)
+    shortSamples[i] = 0;
 
-        for (uint8_t i = 0; i < LONG_SIZE; i++)
-            longSamples[fsr][i] = 0;
-    }
+  for (uint8_t i = 0; i < LONG_SIZE; i++)
+    longSamples[i] = 0;
 
-    for (uint8_t fsr = 0; fsr < 3; fsr++)
-        lastLongTime[fsr] = millis();
+  lastLongTime = millis();
 }
 
 //
@@ -77,85 +65,73 @@ void InitValues()
 //
 void setup()
 {
-    InitValues();
+  InitValues();
 
-    for (uint8_t fsr = 0; fsr < 3; fsr++)
-    {
-        pin = fsrPins[fsr];
-        pinMode(pin, INPUT);
-        digitalWrite(pin, HIGH);  // Use the internal pull-up resistors.
-    }
+  pinMode(fsrPin, INPUT);
+  digitalWrite(fsrPin, HIGH);  // Use the internal pull-up resistors.
 
-    // Set the green combined LED to on so it acts as a power-on indicator. We'll turn it
-    // off whenever we trigger the end stop.
-    pinMode(LEDTRIGGER, OUTPUT);
-    digitalWrite(LEDTRIGGER, HIGH);
+  // Set the green combined LED to on so it acts as a power-on indicator. We'll turn it
+  // off whenever we trigger the end stop.
+  pinMode(LEDTRIGGER, OUTPUT);
+  digitalWrite(LEDTRIGGER, HIGH);
 
-    // Set the endstop pin to be an output that is set for NC
-    pinMode(ENDSTOP, OUTPUT);
-    digitalWrite(ENDSTOP, LOW);
+  // Set the endstop pin to be an output that is set for NC
+  pinMode(ENDSTOP, OUTPUT);
+  digitalWrite(ENDSTOP, LOW);
 };
 
 //
 // Captures a new value once LONG_INTERVAL ms have passed since the last sample.
 //
 // Returns: The current long-range average
-uint16_t UpdateLongSamples(short fsr, int avg)
+uint16_t UpdateLongSamples(int avg)
 {
-    unsigned long current = millis();
-    if (current - lastLongTime[fsr] <= LONG_INTERVAL)
-    {
-        return longAverage[fsr];
-    }
+  unsigned long current = millis();
+  if (current - lastLongTime <= LONG_INTERVAL)
+  {
+    return longAverage;
+  }
 
-    longSamples[fsr][longIndex[fsr]++] = avg;
-    if (longIndex[fsr] >= LONG_SIZE)
-    {
-        longIndex[fsr] = 0;
-    }
+  longSamples[longIndex++] = avg;
+  if (longIndex >= LONG_SIZE)
+  {
+      longIndex = 0;
+  }
 
-    uint16_t total = 0;
-    for (int i = 0; i < LONG_SIZE; i++)
-    {
-        total += longSamples[fsr][i];
-    }
+  uint16_t total = 0;
+  for (int i = 0; i < LONG_SIZE; i++)
+  {
+    total += longSamples[i];
+  }
     
-    longAverage[fsr] = total / LONG_SIZE;
+  longAverage = total / LONG_SIZE;
 
-    lastLongTime[fsr] = millis();
-    return longAverage[fsr];
+  lastLongTime = millis();
+  return longAverage;
 }
 
-void CalculateThreshold(short fsr)
+void CalculateThreshold()
 {
-    uint16_t avg = 0;
-    for (int i = 0; i < SHORT_SIZE; i++)
-    {
-        avg += shortSamples[fsr][i];
-    }
-    uint16_t value = avg / SHORT_SIZE;
-
-    uint16_t longAverage = UpdateLongSamples(fsr, value);
-
-    uint16_t threshold = 0.92 * longAverage;
-
-    bool triggered = value < threshold;
-    SetOutput(fsr, triggered);
+  uint16_t avg = 0;
+  for (int i = 0; i < SHORT_SIZE; i++)
+  {
+    avg += shortSamples[i];
+  }
+  uint16_t value = avg / SHORT_SIZE;
+  uint16_t longAverage = UpdateLongSamples(value);
+  uint16_t threshold = 0.85 * longAverage;
+  bool triggered = value < threshold;
+  SetOutput(triggered);
 }
 
 void loop()
 {
-//    uint8_t fsr = 8;
-    for (uint8_t fsr = 0; fsr < 3; fsr++)
-    {
-        int value = analogRead(fsrAnalogNum[fsr]);
-
-        shortSamples[fsr][averageIndex[fsr]++] = value;
-        if (averageIndex[fsr] >= SHORT_SIZE)
-        {
-            CalculateThreshold(fsr);
-            averageIndex[fsr] = 0;
-        }
-    }
+  int value = analogRead(fsrAnalogNum);
+  shortSamples[averageIndex++] = value;
+  if (averageIndex >= SHORT_SIZE)
+  {
+    CalculateThreshold();
+    averageIndex = 0;
+  }
 };
 
